19c895e67bc1497e633ff1425e4a90479579e53f
Chand2006rr <chandmishra944@gmail.com>
2026-08-27 15:32:57 +0530

    Time: 0 ms (100%), Space: 42.3 MB (15.94%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int fib(int n) {
+
+        if(n<=1) return n;
+    int dp[] = new int[n+1];
+       dp[0] = 0;
+       dp[1] = 1;
+       for(int i=2;i<=n;i++){
+        dp[i] = dp[i-1] + dp[i-2];
+       }
+       return dp[n];
+
+
+        // tabular method dp
+        // if(n==0 || n==1){
+        //     return n;
+        // }
+    //    int dp[] = new int[n+1];
+    //    dp[0] = 0;
+    //    dp[1] = 1;
+    //    for(int i=2;i<=n;i++){
+    //     dp[i] = dp[i-1] + dp[i-2];
+    //    }
+    //    return dp[n];
+
+
+
+    //   Recursion
+    // if(n==0 || n==1) return n;
+    // return fib(n-1)+fib(n-2);
+    }
+}
