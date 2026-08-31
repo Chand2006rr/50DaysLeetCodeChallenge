a9e00f9d6feda93ffa6ed29a0ef55c55b7866c22
Chand2006rr <chandmishra944@gmail.com>
2026-08-31 15:54:26 +0530

    Time: 20 ms (78.54%), Space: 53.9 MB (69.16%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,17 +1,54 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int n=text1.length();
+        //there is no need to make a function for tabulation 
+
+        int n = text1.length();
         int m = text2.length();
         int dp[][] = new int[n+1][m+1];
 
         for(int i=0;i<n+1;i++){
             for(int j=0;j<m+1;j++){
-                dp[i][j] = -1;
+                if(i==0 || j==0){
+                    dp[i][j] = 0;
+                }
             }
         }
 
-        return lcs2(text1,text2,n,m,dp);
-    }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }else{
+                    int ans1 = dp[i-1][j];
+                    int ans2 = dp[i][j-1];
+                    dp[i][j] = Math.max(ans1,ans2);
+                }
+            }
+        }
+
+        return dp[n][m];
+
+            //   Recusion
+        // int n=text1.length();
+        // int m = text2.length();
+        // // return lcs(text1,text2,n,m);
+
+
+                    // Memoization
+    //     int n=text1.length();
+    //     int m = text2.length();
+    //     int dp[][] = new int[n+1][m+1];
+
+    //     for(int i=0;i<n+1;i++){
+    //         for(int j=0;j<m+1;j++){
+    //             dp[i][j] = -1;
+    //         }
+    //     }
+
+    //     return lcs2(text1,text2,n,m,dp);
+    // }
+
+                        // Recursion
 
     // public int lcs(String text1, String text2, int n, int m){
     //     if(n==0 || m==0){
@@ -27,22 +64,26 @@ class Solution {
     // }
 
 
-    public int lcs2(String text1, String text2, int n, int m,int dp[][]){
-        if(n==0 || m==0){
-            return 0;
-        }
+                            // Memoization
+                            
+    // public int lcs2(String text1, String text2, int n, int m,int dp[][]){
+    //     if(n==0 || m==0){
+    //         return 0;
+    //     }
         
-        if(dp[n][m] != -1){
-            return dp[n][m];
-        }
+    //     if(dp[n][m] != -1){
+    //         return dp[n][m];
+    //     }
 
-        if(text1.charAt(n-1) == text2.charAt(m-1)){
-            return dp[n][m] = lcs2(text1,text2,n-1,m-1,dp)+1;
-        }else{
-            int ans1 = lcs2(text1,text2,n-1,m,dp);
-            int ans2 = lcs2(text1,text2,n,m-1,dp);
-            dp[n][m] = Math.max(ans1,ans2);
-            return dp[n][m];
-        }
+    //     if(text1.charAt(n-1) == text2.charAt(m-1)){
+    //         return dp[n][m] = lcs2(text1,text2,n-1,m-1,dp)+1;
+    //     }else{
+    //         int ans1 = lcs2(text1,text2,n-1,m,dp);
+    //         int ans2 = lcs2(text1,text2,n,m-1,dp);
+    //         dp[n][m] = Math.max(ans1,ans2);
+    //         return dp[n][m];
+    //     }
+    // }
+
     }
 }
